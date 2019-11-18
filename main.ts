d43602a0c2ddede107dca1c83d687764e2cab0c4
canonreeves <39296113+canonreeves@users.noreply.github.com>
2019-11-17 23:24:18 -0600

    Added Backwards and an icon

--- main.ts
+++ main.ts
@@ -7,21 +7,36 @@ namespace MOREbot {
     /**
      * Controls for the MOREbot Base Robot
      */
-    
-    
-    //% block="Go forward at $speed \\%"
+
+
+    //% block="Go forward at $speed speed\\%"
     //% speed.shadow="speedPicker"
-    //% block
     //% speed.min=0 speed.max= 100 x.defl=50
     export function forward(speed: number) {
+        speed = Math.abs(speed);
         speed *= 10;
-        
+
         pins.analogWritePin(AnalogPin.P1, speed)
         pins.digitalWritePin(DigitalPin.P13, 1)
         pins.digitalWritePin(DigitalPin.P12, 0)
         pins.analogWritePin(AnalogPin.P2, speed)
         pins.digitalWritePin(DigitalPin.P15, 1)
         pins.digitalWritePin(DigitalPin.P16, 0)
+    }
+
+    //% block="Go backward at $speed speed\\%"
+    //% speed.shadow="speedPicker"
+    //% speed.min=0 speed.max= 100 x.defl=50
+    export function Backward(speed: number) {
+        speed = Math.abs(speed);
+        speed *= 10;
+
+        pins.analogWritePin(AnalogPin.P1, speed)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.analogWritePin(AnalogPin.P2, speed)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
     }
 
 
